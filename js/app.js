29c9d8e7dfabcfe8f56f6eb1ab2a59607f8c6053
Andrew Rich <andrew_rich@hotmail.com>
2016-07-03 21:22:38 -0400

    p's added, still not right

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
   $('.ryu').mouseenter(function() {
     $(".ryu-still").hide();
     $(".ryu-ready").show();
@@ -9,17 +10,54 @@ $(document).ready(function() {
 	})
 
 .mousedown(function(){
+	playHadouken();
 	$(".ryu-ready").hide();
 	$(".ryu-throwing").show();
-	$(".hadouken").show();
+	$(".hadouken").finish().show().animate(
+		{"left":"1020px"},400,
+		function(){
+			$(this).hide();
+			$(this).css("left","520px");
+		});
 	})
 .mouseup(function(){
 	$(".ryu-throwing").hide();
 	$(".ryu-ready").show();
 	});
 
+$(document).keydown(function(e) {
+        if (e.keyCode == 88) {
+            $('.ryu-still').hide();
+            $(".ryu-ready").hide();
+            $('.ryu-cool').show();
+        }  
+    })
+    	.keyup(function(e) {
+        if (e.keyCode == 88) {
+            $('.ryu-cool').hide();
+            $('.ryu-still').show();
+        }
+    });
+// cant go back to standing still w/o adding a ryu
+$(function(){
+		$("#first-p").fadeIn(3000);
+		$("#second-p").fadeIn(7000);
+		});
+
+
+
+
 
 });
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
+
+
+
 
 
 
