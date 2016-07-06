bb627aa4691de1043b81b82d5a2bbd48aaa69ad5
Andrew Rich <andrew_rich@hotmail.com>
2016-07-06 11:49:07 -0400

    fixed double ryu issue

--- js/app.js
+++ js/app.js
@@ -1,48 +1,51 @@
 $(document).ready(function() {
 
-  $('.ryu').mouseenter(function() {
-    $(".ryu-still").hide();
-    $(".ryu-ready").show();
-  	})/* no semicolon due to chaining of mouseleave*/ 
-.mouseleave(function(){
-	$(".ryu-still").show();
-	$(".ryu-ready").hide();
-	})
+   $('.ryu').mouseenter(function() {
+       $(".ryu-still").hide();
+       $(".ryu-ready").show();
+   }) /* no semicolon due to chaining of mouseleave*/
+       .mouseleave(function() {
+           $(".ryu-still").show();
+           $(".ryu-ready").hide();
+       })
 
-.mousedown(function(){
-	playHadouken();
-	$(".ryu-ready").hide();
-	$(".ryu-throwing").show();
-	$(".hadouken").finish().show().animate(
-		{"left":"1020px"},400,
-		function(){
-			$(this).hide();
-			$(this).css("left","520px");
-		});
-	})
-.mouseup(function(){
-	$(".ryu-throwing").hide();
-	$(".ryu-ready").show();
-	});
+   .mousedown(function() {
+       playHadouken();
+       $(".ryu-ready").hide();
+       $(".ryu-still").hide();
+       $(".ryu-throwing").show();
+       $(".hadouken").finish().show().animate({
+               "left": "1020px"
+           }, 400,
+           function() {
+               $(this).hide();
+               $(this).css("left", "520px");
+           });
+   })
+       .mouseup(function() {
+           $(".ryu-throwing").hide();
+           $(".ryu-still").hide();
+           $(".ryu-ready").show();
+       });
 
-$(document).keydown(function(e) {
-        if (e.keyCode == 88) {
-            $('.ryu-still').hide();
-            $(".ryu-ready").hide();
-            $('.ryu-cool').show();
-        }  
-    })
-    	.keyup(function(e) {
-        if (e.keyCode == 88) {
-            $('.ryu-cool').hide();
-            $('.ryu-still').show();
-        }
-    });
-// cant go back to standing still w/o adding a ryu
-$(function(){
-		$("#first-p").fadeIn(3000);
-		$("#second-p").fadeIn(7000);
-		});
+   $(document).keydown(function(e) {
+       if (e.keyCode == 88) {
+           $('.ryu-still').hide();
+           $(".ryu-ready").hide();
+           $('.ryu-cool').show();
+       }
+   })
+       .keyup(function(e) {
+           if (e.keyCode == 88) {
+               $('.ryu-cool').hide();
+               $('.ryu-still').show();
+           }
+       });
+   // cant go back to standing still w/o adding a ryu
+   $(function() {
+       $("#first-p").fadeIn(3000);
+       $("#second-p").fadeIn(7000);
+   });
 
 
 
@@ -50,13 +53,11 @@ $(function(){
 
 });
 
-function playHadouken () {
-  $('#hadouken-sound')[0].volume = 0.5;
-  $('#hadouken-sound')[0].load();
-  $('#hadouken-sound')[0].play();
+function playHadouken() {
+   $('#hadouken-sound')[0].volume = 0.5;
+   $('#hadouken-sound')[0].load();
+   $('#hadouken-sound')[0].play();
 }
-
-
 
 
 
